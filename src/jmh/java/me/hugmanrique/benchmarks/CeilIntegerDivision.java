fce845cb9918e17416a0943b1b6fa691b7c8c923
Hugo Manrique <contact@hugmanrique.me>
2020-03-22 14:32:10 +0100

    Remove random number generation from benchmark

--- src/jmh/java/me/hugmanrique/benchmarks/CeilIntegerDivision.java
+++ src/jmh/java/me/hugmanrique/benchmarks/CeilIntegerDivision.java
@@ -1,7 +1,14 @@
 package me.hugmanrique.benchmarks;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -11,22 +18,31 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
  * Benchmarks the different options on <a href="https://stackoverflow.com/a/21830188/4514467">this
  * great post</a>.
  *
- * Benchmark                     Mode  Cnt         Score        Error  Units
- * CeilIntegerDivision.option1  thrpt   25  94149045,475   761963,028  ops/s
- * CeilIntegerDivision.option2  thrpt   25  94328653,910   782928,022  ops/s
+ * Benchmark                    Mode  Cnt  Score   Error  Units
+ * CeilIntegerDivision.option1  avgt   25  3,792   0,027  ns/op
+ * CeilIntegerDivision.option2  avgt   25  3,851   0,013  ns/op
  */
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
 public class CeilIntegerDivision {
 
     private static final Random random = new Random(1);
 
+    private int a;
+    private int b;
+
+    @Setup
+    public void setup() {
+        a = random.nextInt();
+        b = random.nextInt();
+    }
+
     @Benchmark
     public int option1() {
-        int a = random.nextInt();
-        int b = random.nextInt();
-
         if (b == 0) {
             // The actual implementation should throw ArithmeticException
-            return 0;
+            return a;
         }
 
         return a / b + ((a % b == 0) ? 0 : 1);
@@ -34,14 +50,11 @@ public class CeilIntegerDivision {
 
     @Benchmark
     public int option2() {
-        int a = random.nextInt();
-        int b = random.nextInt();
-
         if (a == 0) {
-            return 0;
+            return b;
         } else if (b == 0) {
             // The actual implementation should throw ArithmeticException
-            return 0;
+            return a;
         }
 
         return (a - 1) / b + 1;
